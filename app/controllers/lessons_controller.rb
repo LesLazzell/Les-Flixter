2c984b09cbbcfa638459701577c01b9681343fc8
Les Lazzell <llazzell4664@outlook.com>
2019-06-01 22:09:11 +0000

    flixter 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,13 @@
 class LessonsController < ApplicationController
+    before_action :require_enrolled_in_current_course
+
     def show
+    end
+
+    def require_enrolled_in_current_course
+        if !current_user.enrolled_in?(current_course)
+            redirect_to course_path(current_course), alert: 'Please Enroll First'
+        end
     end
 
     private
@@ -8,4 +16,8 @@ class LessonsController < ApplicationController
     def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
     end
+
+    def current_course
+        current_lesson.section.course
+    end 
 end
